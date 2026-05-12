e273971e41cd9faa2124f4d65a44cac333c1fb61
leizhao <lei@arcsite.com>
2026-05-12 12:00:48 +0800

    feat: update age display in conditional statements for clarity

--- source/01-phase1/main.m
+++ source/01-phase1/main.m
@@ -8,10 +8,13 @@ int main(int argc, const char * argv[]) {
 
         // 2. 条件语句
         if (age >= 18) {
-            NSLog(@"%@ 已经成年了。", name);
+            NSLog(@"%@ 已经成年了, 年龄是 %d", name, age);
         } else {
-            NSLog(@"%@ 还未成年。", name);
+            NSLog(@"%@ 还未成年, 年龄是 %d", name, age);
         }
+
+        NSString *greeting = [NSString stringWithFormat:@"Hello, %@! You are %d years old.", name, age];
+        NSLog(@"%@", greeting);
 
         // 3. 循环语句
         NSLog(@"打印数字 1 到 5：");
